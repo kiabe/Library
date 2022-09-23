509e49c36354c334b16daa81cb7ae1c7ebdfcf5a
Elliot Baik <elbaik412@gmail.com>
2022-09-23 15:05:37 -0500

    Prototype  basic design

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let gameLibrary = [
     {
-        "name": "Multiversus",
-        "genre": "Fighting",
-        "multiplayer": true
+        name: "Multiversus",
+        genre: "Fighting",
+        multiplayer: true
     }
 ];
 
@@ -36,11 +36,48 @@ addGameToLibrary(RE2Make);
 let Valorant = new Game('Valorant', 'Tactical FPS', true);
 addGameToLibrary(Valorant);
 
-// DOM
+// DOM Variable Selectors
 const shelf = document.querySelector('#shelf');
-const gameOne = document.querySelector('.game');
-gameOne.textContent = gameLibrary[0].name + ' ' + gameLibrary[0].genre + ' ' + 'is multiplayer? : ' + gameLibrary[0].multiplayer;
+const gameCaseCount = document.querySelectorAll('.game');
+const button = document.querySelector('#button');
+
+// DOM Stuff
+function createGameCase() {
+    const gameCase = document.createElement('div');
+    gameCase.classList.add('game');
+    shelf.appendChild(gameCase);
+}
+
+function labelGameCase() {
+    // Prints on to div with class name 'game' that label cases with the following: name, genre, and whether it is multiplayer or not
+    // Loops through gameLibrary array and prints content of object to gameCase div
+
+    for (let i = 0; i < gameCaseCount.length; i++) {
+        const ul = document.createElement('ul');
+        gameCaseCount[i].appendChild(ul);
+        for (let j = 0; j < 20; j++) {
+            // print content of gameLibrary to divs in gameCase
+            if (j === 0) {
+                const li = document.createElement('li');
+                ul.appendChild(li);
+                li.textContent = "Name of game: " + gameLibrary[i].name;
+            } else if (j === 1) {
+                const li = document.createElement('li');
+                ul.appendChild(li);
+                li.textContent = "Genre: " + gameLibrary[i].genre;
+            } else if (j === 2) {
+                const li = document.createElement('li');
+                ul.appendChild(li);
+                li.textContent = "Is this multiplayer? : " + gameLibrary[i].multiplayer;
+            }
+        }
+    }
+}
+
+labelGameCase();
+
+// DOM Button Stuff
 const newGameButton = document.createElement('button');
 newGameButton.setAttribute('id', 'newGameButton');
 newGameButton.textContent = 'Add New Game to Library';
-shelf.appendChild(newGameButton);
+button.appendChild(newGameButton);
